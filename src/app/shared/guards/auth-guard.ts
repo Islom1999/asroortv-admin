8cccab02c252a3c04346af819cd1ee9180d8727f
Karimov Islom <islomkarimovalisherovich@gmail.com>
2024-06-10 21:28:14 +0500

    change url

--- src/app/shared/guards/auth-guard.ts
+++ src/app/shared/guards/auth-guard.ts
@@ -1,23 +1,23 @@
-// import { Injectable } from '@angular/core';
-// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-// import { Observable } from 'rxjs';
-// import { AuthService } from 'src/app/auth/auth.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../auth/auth.service';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthGuard implements CanActivate {
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
 
-//   constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-//   canActivate(
-//     route: ActivatedRouteSnapshot,
-//     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-//       if (this.authService.isAuthenticated()) {
-//         return true;
-//       } else {
-//         this.router.navigate(['/login']);
-//         return false;
-//       }
-//   }
-// }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      if (this.authService.isAuthenticated()) {
+        return true;
+      } else {
+        this.router.navigate(['/login']);
+        return false;
+      }
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://api.tvtime.uz/api',
+    apiUrl: 'https://bananatv.topsites.uz/api',
 };
