cfaacaa73487084e1d13554717e697a28d75d616
Karimov Islom <71114810+Islom1999@users.noreply.github.com>
2025-05-22 13:29:12 +0500

    angular upgrade 17 to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgxPermissionsModule } from 'ngx-permissions';
 import { AuthInterceptor } from './shared/interceptors/token.interceptor';
 import { ErrorInterceptor } from './shared/interceptors/error.interceptor';
@@ -16,27 +16,19 @@ import { ErrorInterceptor } from './shared/interceptors/error.interceptor';
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    NgxPermissionsModule.forRoot(),
-    HttpClientModule,
-
-    AppRoutingModule,
-    FormsModule,
-
-    // VgCoreModule,
-    // VgControlsModule
-  ],
-  providers: [
-    { provide: NZ_I18N, useValue: en_US },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    provideAnimationsAsync(),
-    provideHttpClient(),
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        NgxPermissionsModule.forRoot(),
+        AppRoutingModule,
+        FormsModule], providers: [
+            { provide: NZ_I18N, useValue: en_US },
+            { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+            { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+            provideAnimationsAsync(),
+            provideHttpClient(),
+            provideHttpClient(withInterceptorsFromDi()),
+        ]
 })
 export class AppModule { }
