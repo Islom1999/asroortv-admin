dac6b022e4111f08c78511a37b1ccc4dff525a3f
Karimov Islom <71114810+Islom1999@users.noreply.github.com>
2025-05-22 13:45:22 +0500

    angular projects upgrade from@17 to@18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { NgxPermissionsModule } from 'ngx-permissions';
 import { AuthInterceptor } from './shared/interceptors/token.interceptor';
 import { ErrorInterceptor } from './shared/interceptors/error.interceptor';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxPermissionsModule } from 'ngx-permissions';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 registerLocaleData(en);
 
@@ -19,16 +19,21 @@ registerLocaleData(en);
     declarations: [
         AppComponent,
     ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
         NgxPermissionsModule.forRoot(),
         AppRoutingModule,
-        FormsModule], providers: [
-            { provide: NZ_I18N, useValue: en_US },
-            { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-            { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-            provideAnimationsAsync(),
-            provideHttpClient(),
-            provideHttpClient(withInterceptorsFromDi()),
-        ]
+        FormsModule
+    ],
+    providers: [
+        { provide: NZ_I18N, useValue: en_US },
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+        // provideAnimationsAsync(),
+        provideAnimations(),
+        provideHttpClient(),
+        provideHttpClient(withInterceptorsFromDi()),
+    ]
 })
 export class AppModule { }
